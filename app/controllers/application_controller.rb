f78bdca62b43b281ac7e28b826321a1d1fd02aa2
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-28 18:57:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,11 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
      @user = User.find_by_username(params[:username])
+ 
      if @user 
        session[:user_id] = @user.id
        @user = Helpers.current_user(session)
+    
         redirect '/account'
      end
     redirect '/error'
