044a8bad9824c93da4c17c4d4d3a780a5c8f4039
Emily Lillie <emilylillie3@gmail.com>
2019-09-08 18:48:10 +0000

    Done.

--- db/migrate/20190908183544_create_users.rb
+++ db/migrate/20190908183544_create_users.rb
@@ -1,3 +1,5 @@
+# if migrating won't work, first make rake work by entering "bundle exec rake -T" into your terminal, then use rake to make a create_table file by entering "rake db:create_migration NAME=create_users"
+
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
    create_table :users do |t|
